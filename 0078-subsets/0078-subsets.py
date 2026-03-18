a985a21673d3ac3f1fa2f2b9ad69c8d1677719d6
Prerana Hadadi <55052678+preranahadadi@users.noreply.github.com>
2026-03-17 19:34:36 -0500

    Time: 0 ms (100%), Space: 19.3 MB (86.3%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,19 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        subset = []
 
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
+        ans = [] 
+        res = [] 
+
+        def dfs(i, res) : 
+            if i  == len(nums) : 
+                ans.append(res)
                 return 
-            
-            # case where we pick the number
-            subset.append(nums[i])
-            dfs(i+1)
 
-            #case where you don't pick the number
-            subset.pop()
-            dfs(i+1)
-        dfs(0)
-        return res
+
+            pick = dfs(i+1, res + [nums[i]]) 
+            nonpick = dfs(i+1, res) 
+
+        dfs(0, res)
+        return ans 
+        
